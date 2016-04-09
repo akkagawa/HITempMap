8c060d7e5a04c227ed221a83d3f6d0dc66bbb7ac
akkagawa <akkagawa@gmail.com>
2016-04-08 19:49:57 -1000

    Added files via upload

--- MLO_processing.R
+++ MLO_processing.R
@@ -1,6 +1,6 @@
 ## MLO_processing.R
 ## Author: Aurora Kagawa-Viviani
-## Date: March 25 2016
+## Date: March 25 2016; revised April 8 2016
 ## Notes: Script for downloading and aggregating Mauna Loa Observatory data
 
 setwd("C:/Users/Aurora/OneDrive/Documents/Projects/TempMapping/MLO")
@@ -79,7 +79,9 @@ download.file(readme, destfile="README.txt")
 # Units are millimeters/hour.
 # Missing values are denoted by -99
 
-## Hourly to Daily
+################################################
+##  Convert HOURLY to DAILY and write to file ##
+################################################
 out<-list()
 files.out<-paste("mlo_daily_", c(1977:2016),".csv", sep="")
 
@@ -136,24 +138,20 @@ print(i)
 write.csv(out[[i]], files.out[i])
 }
 
-## Aggregate to month-year
-### Fields for output:
-## See http://www.esrl.noaa.gov/gmd/dv/site/site.php?code=MLO
+################################################
+##  Aggregate to MONTH-YEAR and write to file ##  20h = valid day
+################################################
+### Utilize same fields for output as ExtractMonthYearv7.R
+##  For station metadata: http://www.esrl.noaa.gov/gmd/dv/site/site.php?code=MLO
 # Station	   = MLO
 # MonthYear	
 # Elevation	 = 3397.00
 # Latitude	 = 19.5362
 # Longitude	 = -155.5763
-
-
-# Tmax	
-# Tmax.obs	
-# Tmin	
-# Tmin.obs	
-# DTR	
-# DTR.obs	
-# Tavg	
-# Tavg.obs
+# Tmax, Tmax.obs	
+# Tmin, Tmin.obs	
+# DTR, DTR.obs	
+# Tavg, Tavg.obs
 
 # Merge all years into single dataset
 for (i in 1:length(out)){
@@ -161,27 +159,27 @@ for (i in 1:length(out)){
   else {daily.raw<-rbind(daily.raw, out[[i]])}
 }
 
-# Set days with missing data to NA
-daily.raw$NAs<-daily.raw$Obs2<20  # or is this too lax?
-daily<-daily.raw[!daily.raw$NAs,]
+# Identify DAYS with insufficient data and remove
+daily.raw$junk<-daily.raw$Obs2<20  # or is this too lax?
+daily<-daily.raw[!daily.raw$junk,]
 
 # Create year-month index
 ym<-paste(daily$year, daily$month, sep="")
 
 # 2m DAILY temp to MONTHLY temp
-Tmin2d     <- tapply(daily$Tmin2, INDEX=ym, FUN=mean, na.rm=T) # avg daily min, exclude NA
-Tmin.obs2d <- tapply(daily$Tmin2, INDEX=ym, FUN=length)-    #number obs (incl NA)
+Tmin2m     <- tapply(daily$Tmin2, INDEX=ym, FUN=mean, na.rm=T) # avg daily min, exclude NA
+Tmin.obs2m <- tapply(daily$Tmin2, INDEX=ym, FUN=length)-    #number obs (incl NA)
                 tapply(daily$Tmin2, INDEX=ym, FUN=numNA)    #number NAs
-Tmax2d     <- tapply(daily$Tmax2, INDEX=ym, FUN=mean, na.rm=T)
-Tmax.obs2d <- tapply(daily$Tmax2, INDEX=ym, FUN=length)-    
+Tmax2m     <- tapply(daily$Tmax2, INDEX=ym, FUN=mean, na.rm=T)
+Tmax.obs2m <- tapply(daily$Tmax2, INDEX=ym, FUN=length)-    
                 tapply(daily$Tmax2, INDEX=ym, FUN=numNA)    
 
-DTR2d      <- tapply(daily$DTR2, INDEX=ym, FUN=mean, na.rm=T)
-DTR.obs2d  <- tapply(daily$DTR2, INDEX=ym, FUN=length)-    
+DTR2m      <- tapply(daily$DTR2, INDEX=ym, FUN=mean, na.rm=T)
+DTR.obs2m  <- tapply(daily$DTR2, INDEX=ym, FUN=length)-    
                 tapply(daily$DTR2, INDEX=ym, FUN=numNA)    
 
-Tavg2d     <- tapply(daily$Tavg2, INDEX=ym, FUN=mean, na.rm=T)
-Tavg.obs2d  <- tapply(daily$Tavg2, INDEX=ym, FUN=length)-    
+Tavg2m     <- tapply(daily$Tavg2, INDEX=ym, FUN=mean, na.rm=T)
+Tavg.obs2m  <- tapply(daily$Tavg2, INDEX=ym, FUN=length)-    
                 tapply(daily$Tavg2, INDEX=ym, FUN=numNA)    
 
 monthyear<-data.frame(Station ="MLO",
@@ -189,13 +187,55 @@ monthyear<-data.frame(Station ="MLO",
                       Elevation = 3397.00,
                       Latitude	= 19.5362,
                       Longitude	= -155.5763,
-                      Tmax      = Tmax2d,	
-                      Tmax.obs  = Tmax.obs2d,
-                      Tmin      = Tmin2d,
-                      Tmin.obs  = Tmin.obs2d,
-                      DTR	      = DTR2d,
-                      DTR.obs   = DTR.obs2d,	
-                      Tavg      = Tavg2d,
-                      Tavg.obs  = Tavg.obs2d)
+                      Tmax      = Tmax2m,	
+                      Tmax.obs  = Tmax.obs2m,
+                      Tmin      = Tmin2m,
+                      Tmin.obs  = Tmin.obs2m,
+                      DTR	      = DTR2m,
+                      DTR.obs   = DTR.obs2m,	
+                      Tavg      = Tavg2m,
+                      Tavg.obs  = Tavg.obs2m)
 
 write.csv(monthyear, file="monthly_MLO.csv", row.names=F)
+
+############################################
+##  Aggregate to YEARLY and write to file ##  25d = valid month
+############################################
+
+# Identify MONTHS with insufficient data and remove
+monthyear$junk<-monthyear$DTR.obs<25  #stiffer criteria than PRISM's 25% missing
+my<-monthyear[!monthyear$junk,]
+
+# Create yearly index
+y<-substr(my$MonthYear, start=1, stop=4)
+
+Tmin     <- tapply(my$Tmin, INDEX=y, FUN=mean, na.rm=T) # avg min, exclude NA
+Tmin.obs <- tapply(my$Tmin, INDEX=y, FUN=length)-       #number obs (incl NA)
+             tapply(my$Tmin, INDEX=y, FUN=numNA)        #number NAs
+Tmax     <- tapply(my$Tmax, INDEX=y, FUN=mean, na.rm=T)
+Tmax.obs <- tapply(my$Tmax, INDEX=y, FUN=length)-    
+             tapply(my$Tmax, INDEX=y, FUN=numNA)    
+
+DTR      <- tapply(my$DTR, INDEX=y, FUN=mean, na.rm=T)
+DTR.obs  <- tapply(my$DTR, INDEX=y, FUN=length)-    
+             tapply(my$DTR, INDEX=y, FUN=numNA)    
+
+Tavg     <- tapply(my$Tavg, INDEX=y, FUN=mean, na.rm=T)
+Tavg.obs <- tapply(my$Tavg, INDEX=y, FUN=length)-    
+             tapply(my$Tavg, INDEX=y, FUN=numNA)  
+
+year<-data.frame(Station ="MLO",
+                      Year = names(Tmin),
+                      Elevation = 3397.00,
+                      Latitude	= 19.5362,
+                      Longitude	= -155.5763,
+                      Tmax      = Tmax,	
+                      Tmax.obs  = Tmax.obs,
+                      Tmin      = Tmin,
+                      Tmin.obs  = Tmin.obs,
+                      DTR	      = DTR,
+                      DTR.obs   = DTR.obs,	
+                      Tavg      = Tavg,
+                      Tavg.obs  = Tavg.obs)
+
+write.csv(year, file="yearly_MLO.csv", row.names=F)
