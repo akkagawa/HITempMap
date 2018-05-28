48812be1616db23237543989040017c91a877d83
akkagawa <akkagawa@gmail.com>
2018-05-27 20:45:39 -1000

    Add files via upload
    Scripts preparing temperature station data for spatial and temporal analyses

---
+++ Aggregation_and_QAQC.R
@@ -0,0 +1,142 @@
+## Aggregation_and_QAQC.R
+## Author: Aurora Kagawa-Viviani
+## Date: April 17-18 2016; re-run 6/17/2017; re-run 5/6/2018
+##########################################################
+## Match station locations with gridded correlates
+## Subset appropriately for analysis: by month-years or years
+##  Drawn from NCDCplusHaleNet3.R, SubsettingMonthYears1.R
+
+setwd("C:/Users/Aurora/OneDrive/Documents/Projects/TempMapping")
+key<-read.csv("NCDCStationKeys/StationKey_allMay2018.csv")
+
+ghcn_mo<-read.csv("NCDC_DailyTemp/monthly/monthly_mrg_completeNCDCstns.csv")
+ghcn_yr<-read.csv("NCDC_DailyTemp/yearly/yearly_mrg_completeNCDCstns.csv")
+
+MLO_mo<-read.csv("MLO/monthly_MLO.csv")
+MLO_yr<-read.csv("MLO/yearly_MLO.csv")
+
+################## HaleNet not yet aggregated ##################
+filesmo<-list.files("HaleNet/May2018/monthly")
+filesyr<-list.files("HaleNet/May2018/yearly")
+
+for (i in 1:length(filesmo)){
+  mo<-read.csv(paste("HaleNet/May2018/monthly/", filesmo[i], sep=""))
+  yr<-read.csv(paste("HaleNet/May2018/yearly/", filesyr[i], sep=""))
+  if(i==1){
+    HN_mo<-mo
+    HN_yr<-yr
+  }else{
+    HN_mo<-rbind(HN_mo, mo)
+    HN_yr<-rbind(HN_yr, yr)}
+}
+write.csv(HN_mo, "HaleNet/May2018/monthlyHN_all.csv", row.names=F)
+write.csv(HN_yr, "HaleNet/May2018/yearlyHN_all.csv", row.names=F)
+##################################################################
+## Aggregate data from multiple sources and compare to StationKey
+## Read in monthly and annual series data
+monthly<-rbind(ghcn_mo, HN_mo[,-14], MLO_mo)
+yearly<-rbind(ghcn_yr, HN_yr[,-14], MLO_yr)
+
+## Rename the "ID" column in the station key to "Station"
+corrkey<-key[,c(1:2, 10, 12:14)]; names(corrkey)[1]<-"Station"
+
+## Merge data and the key files, matching by "Station"
+merged_mo<-merge(monthly, corrkey, by="Station")
+merged_yr<-merge(yearly, corrkey, by="Station")
+
+## Split MonthYear column into "Month" and "Year"
+merged_mo$Month<-substr(merged_mo$MonthYear, start=5, stop=6)
+merged_mo$Year<-substr(merged_mo$MonthYear, start=1, stop=4)
+##################################################################
+## QA/QC by completeness: Drop incomplete observations
+merged_mo_crit<-droplevels(subset(merged_mo, Tmax.obs>=25))  # keep only months with 25 days
+   ## Note the drop from 311 stations to 289
+
+merged_yr_crit<-droplevels(subset(merged_yr, Tmax.obs==12))  # keep only full station-years
+  ## Note the drop from 311 stations to 219
+
+write.csv(merged_mo_crit, "Merged_mo.csv", row.names=F)
+write.csv(merged_yr_crit, "Merged_yr.csv", row.names=F)
+##################################################################
+## QA/QC by station: 
+##   1) plot each station time-series
+##   2) visually inspect for obvious outliers
+##   3) drop stations that provide spurious data?  Or set some criteria?
+
+start_mo<-tapply(merged_mo_crit$MonthYear, merged_mo_crit$Station, min)
+end_mo<-tapply(merged_mo_crit$MonthYear, merged_mo_crit$Station, max)
+
+# Convert to Date class; R doesn't like YearMonth only-needs Day
+z.st<-strptime(paste(start_mo, "-01", sep=""), "%Y%m-%d") 
+z.end<-strptime(paste(end_mo, "-01", sep=""), "%Y%m-%d")
+
+### Create a series of date/times at defined intervals
+for (i in 1:length(start_mo)){
+  times_mo<-seq.POSIXt(from=z.st[i], to=z.end[i], by="month") # create series
+  times_yr<-seq.POSIXt(from=z.st[i], to=z.end[i], by="year") # create series
+  stn_mo<-rep(names(start_mo)[i], times=length(times_mo))
+  stn_yr<-rep(names(start_mo)[i], times=length(times_yr))
+  filled_mo<-data.frame(Station=stn_mo,
+                       MonthYear=paste(substr(times_mo, start=1, stop=4),
+                                       substr(times_mo, start=6, stop=7),
+                                       sep=""))
+  filled_yr<-data.frame(Station=stn_yr,
+                       Year=substr(times_yr, start=1, stop=4))
+  if (i==1){
+    timesFill_mo<-filled_mo
+    timesFill_yr<-filled_yr
+  }else{
+    timesFill_mo<-rbind(timesFill_mo, filled_mo)
+    timesFill_yr<-rbind(timesFill_yr, filled_yr)
+  }
+}
+
+## Merge the even time series with the data to create ts objects
+timesFill_mo$Station<-as.character(timesFill_mo$Station)
+timesFill_mo$MonthYear<-as.numeric(as.character(timesFill_mo$MonthYear))
+
+timesFill_yr$Station<-as.character(timesFill_yr$Station)
+timesFill_yr$Year<-as.numeric(as.character(timesFill_yr$Year))
+
+filled_mo<-merge(timesFill_mo, merged_mo, by=c("Station", "MonthYear"), all=T)
+filled_yr<-merge(timesFill_yr, merged_yr, by=c("Station", "Year"), all=T)
+
+## Then plot.ts at monthly and annual frequencies
+##  From PlotMonthlybyStation.R (Aug 2015)
+ts_mo_stn<-list()
+stnkey<-unique(filled_mo$Station)
+pdf(file="QAQCstationplots.pdf", width = 10, height = 7)
+par(mfrow=c(4,2), mar=c(2,3,2,1)) ## 8 plots per page; save as multipage pdf
+    
+for (i in 1:length(stnkey)){
+  
+  ## Subset and organize time series data
+  sub<-subset(filled_mo, filled_mo$Station==stnkey[i])
+  start<-as.numeric(c(sub$Year[1], sub$Month[1]))
+  end<-as.numeric(c(sub$Year[dim(sub)[1]], sub$Month[dim(sub)[1]]))
+  ts_Tmax<-ts(sub$Tmax, start=start, end=end,freq=12)
+  ts_Tmin<-ts(sub$Tmin, start=start, end=end, freq=12)
+  ts_DTR<-ts(sub$DTR, start=start, end=end, freq=12)
+  ts_Tavg<-ts(sub$Tavg, start=start, end=end, freq=12)
+  
+  ts_mo_stn[[i]]<-list(ts_Tmax, ts_Tmin, ts_DTR, ts_Tavg)
+  
+  ## Prepare for plotting
+  tslims<-c(min(ts_DTR, ts_Tmin, na.rm=T),max(ts_Tmax, na.rm=T))
+  plot(ts_Tmax, ylim=tslims, col="red",
+       main=paste(stnkey[i], sub$Name[1],
+                  "Elev(m)", sub$CorrElev_m[1]))
+  lines(ts_Tmin, col="blue")
+  lines(ts_Tavg, col="black", lty=2)
+  lines(ts_DTR, col="black", lty=3)
+  
+  print(i)
+}
+
+dev.off()
+    
+##################################################################
+##  Some station inhomogeneities now visible (drift, etc) and gaps/poor coverage
+##  We will proceed, though, without dropping these out.
+##################################################################
+
---
+++ StationKeyProcessing3.R
@@ -0,0 +1,268 @@
+## StationKey_processing3.R
+## Author: Aurora Kagawa-Viviani
+## Date: Apr 13-16 2016; re-run 6/17/2017; re-run 5/6/2018
+## Notes: Script for organizing station metadata and generating a final station key
+
+########################################################################
+## NCDC                                                              ###
+########################################################################
+# use "monthly/monthly_mrg_completeNCDCstns.csv" for 299 stations with Tmax/Tmin data
+# use "ghcnd-stationsHI.csv" for list of 688 HI stations downloaded: lat, long, elev, name
+# use "ncdcstation2.csv" for parsed mshr.txt metadata and station relocations
+
+setwd("C:/Users/Aurora/OneDrive/Documents/Projects/TempMapping/NCDC_DailyTemp")
+
+### Match everything else only to the stations used in the monthly Temp dataset
+monthly<-read.csv("monthly/monthly_mrg_completeNCDCstns.csv")
+Tghcnd<-unique(monthly$Station)             # 299 values
+SK<-data.frame(ghcnd_id = Tghcnd,
+               start = tapply(monthly$MonthYear, monthly$Station, min),
+               end   = tapply(monthly$MonthYear, monthly$Station, max),
+               Tmin  = tapply(monthly$Tmin, monthly$Station, mean),
+               Tmax  = tapply(monthly$Tmax, monthly$Station, mean),
+               DTR   = tapply(monthly$DTR, monthly$Station, mean),
+               Tavg  = tapply(monthly$Tavg, monthly$Station, mean),
+               Obs   = tapply(monthly$Tavg, monthly$Station, length))
+               
+
+allghcnd<-read.csv("ghcnd-stationsHI.csv", strip.white=T)     # 688   5      ghcnd = "ID"
+# rename column for merging
+names(allghcnd)[grep("ID", names(allghcnd))]<-"ghcnd_id"  
+names(allghcnd)[grep("elev", names(allghcnd))]<-"elev.m"
+
+#### MERGE dataframes SK (299), allghcnd (688)
+merged<-merge(SK, allghcnd, by="ghcnd_id", all=F)   # take ONLY 299 stations of interest
+write.csv(merged, file="mergedNCDCstation299.csv", row.names=F)
+
+#### Optional: Compare to metadata on station movements, etc: mshr(2749)  
+mshr<-read.csv("ncdcstation2.csv") # 2749   17    ghcnd = "ghcnd_id"  NO Midway
+meta<-list()
+for (i in 1:length(Tghcnd)){
+  meta[[i]]<-subset(mshr, as.character(mshr$ghcnd_id)==as.character(Tghcnd[i]))
+  
+  # look at only most recent coordinates
+  if (i==1){metaLast<-meta[[i]][dim(meta[[i]])[1],]
+  }else{metaLast<-rbind(metaLast, meta[[i]][dim(meta[[i]])[1],])}
+} 
+
+mergedmshr<-merge(merged, metaLast, by="ghcnd_id")
+write.csv(mergedmshr, file="merged_mshr_NCDCstation298.csv", row.names=F)  
+
+##################################################################
+## FindingSKNs.R
+## Goal: checking NCDC station locations
+## Script to match station names with State Key Numbers (SKN) values
+##    documented elsewhere by Abby Frazier and/or Jon Eischeid
+## Script used exact matching ("which") and fuzzy matching ("agrep")
+
+## Step1 following this is manual examination of NCDC station names that do not match other files
+## Step2 following this is to extract lat/long/elevation from the other files
+##    in some cases, NCDC locations are more precise; keep these
+##    in other cases, NCDC locations are off; use the RF atlas corrected values here
+
+###############################################################################
+## open mergedNCDCstation299.csv (merged_mshr_NCDCstation299.csv) in excel, ##
+###   and save to drop quotes                                                ## 
+###############################################################################
+dat<-read.csv("merged_mshr_NCDCstation298_rmwhtspc.csv", strip.white = T, stringsAsFactors=F)  
+
+ghcn<-as.character(dat$ghcnd_id) # create a list of unique ghcnd IDs
+NCDC<-dat$name              # use these names
+
+#Identify RAWS
+#remove "HAWAII" to aid in string fuzzy matching
+raws     <-which(substr(dat$ghcnd_id, start=1, stop=3)=="USR")
+rawsHI<-substr(NCDC[raws], start=1, stop=nchar(NCDC[raws])-7)  
+NCDCshort<-NCDC; NCDCshort[raws]<-rawsHI
+  
+## match NCDCshort to SKN
+
+ncdcSKN     <-read.csv("../NCDCStationKeys/ncdc_hedr2_rmwhtspc.csv", stringsAsFactors = F); SKN<-vector("character") # initial ncdc_hedr.csv was garbled
+tempSKN     <-read.csv("../NCDCStationKeys/TempHedrCurr.csv", stringsAsFactors = F); SKN2<-vector("character")
+JonSKN      <-read.csv("../NCDCStationKeys/JonMonthlyHedr.csv", stringsAsFactors = F); SKN3<-vector("character")
+rfhedrSKN   <-read.csv("../NCDCStationKeys/RF_Hedr_4.13.11.csv", stringsAsFactors = F); SKN4<-vector("character")
+rf2015SKN   <-read.csv("../NCDCStationKeys/RFFinalStationData_Used_201504pr.csv", stringsAsFactors = F); SKN5<-vector("character")
+rfun2015SKN <-read.csv("../NCDCStationKeys/RFFinalStations_NotUsed_201504pr.csv", stringsAsFactors = F); SKN6<-vector("character")
+
+#### EXACT MATCHES in name
+for (i in 1:length(NCDCshort)){
+  SKNindex<-which(ncdcSKN$Name==NCDCshort[i])
+  if (length(SKNindex)!=0){SKN[i]<-paste(ncdcSKN$SKN[SKNindex], collapse="/")
+  }else{SKN[i]<-NA}
+  
+  SKN2index<-which(tempSKN$Name==NCDCshort[i])
+  if (length(SKN2index)!=0){SKN2[i]<-paste(tempSKN$SKN[SKN2index], collapse="/")
+  }else{SKN2[i]<-NA}
+  
+  SKN3index<-which(JonSKN$Name==NCDCshort[i])
+  if (length(SKN3index)!=0){SKN3[i]<-paste(JonSKN$SKN[SKN3index], collapse="/")
+  }else{SKN3[i]<-NA}
+  
+  SKN4index<-which(rfhedrSKN$Name==NCDCshort[i])
+  if (length(SKN4index)!=0){SKN4[i]<-paste(rfhedrSKN$SKN[SKN4index], collapse="/")
+  }else{SKN4[i]<-NA}
+  
+  SKN5index<-which(rf2015SKN$Name==NCDCshort[i])
+  if (length(SKN5index)!=0){SKN5[i]<-paste(rf2015SKN$SKN[SKN5index], collapse="/")
+  }else{SKN5[i]<-NA}
+  
+  SKN6index<-which(rfun2015SKN$Name==NCDCshort[i])
+  if (length(SKN6index)!=0){SKN6[i]<-paste(rfun2015SKN$SKN[SKN6index], collapse="/")
+  }else{SKN6[i]<-NA}
+  
+  print(i)
+}
+
+#### FUZZY MATCHING
+SKNa<-vector("character")
+SKN2a<-vector("character")
+SKN3a<-vector("character")
+SKN4a<-vector("character")
+SKN5a<-vector("character")
+SKN6a<-vector("character")
+
+for (i in 1:length(NCDCshort)){
+  SKNaindex<-agrep(NCDCshort[i], ncdcSKN$Name, max.distance=0.1, ignore.case=T)
+  if (length(SKNaindex)!=0){SKNa[i]<-paste(ncdcSKN$SKN[SKNaindex], collapse="/")}
+  else{SKNa[i]<-NA}
+  
+  SKN2aindex<-agrep(NCDCshort[i], tempSKN$Name, max.distance=0.1, ignore.case=T)
+  if (length(SKN2aindex)!=0){SKN2a[i]<-paste(tempSKN$SKN[SKN2aindex], collapse="/")}
+  else{SKN2a[i]<-NA}
+  
+  SKN3aindex<-agrep(NCDCshort[i], JonSKN$Name, max.distance=0.1, ignore.case=T)
+  if (length(SKN3aindex)!=0){SKN3a[i]<-paste(JonSKN$SKN[SKN3aindex], collapse="/")}
+  else{SKN3a[i]<-NA}
+  
+  SKN4aindex<-agrep(NCDCshort[i], rfhedrSKN$Name, max.distance=0.1, ignore.case=T)
+  if (length(SKN4aindex)!=0){SKN4a[i]<-paste(rfhedrSKN$SKN[SKN4aindex], collapse="/")}
+  else{SKN4a[i]<-NA}
+  
+  SKN5aindex<-agrep(NCDCshort[i], rf2015SKN$Name, max.distance=0.1, ignore.case=T)
+  if (length(SKN5aindex)!=0){SKN5a[i]<-paste(rf2015SKN$SKN[SKN5aindex], collapse="/")}
+  else{SKN5a[i]<-NA}
+  
+  SKN6aindex<-agrep(NCDCshort[i], rfun2015SKN$Name, max.distance=0.1, ignore.case=T)
+  if (length(SKN6aindex)!=0){SKN6a[i]<-paste(rfun2015SKN$SKN[SKN6aindex], collapse="/")}
+  else{SKN6a[i]<-NA}
+  
+  print(i)
+}
+
+out<-cbind(Station=ghcn,
+           NCDCname=NCDCshort,
+           ncdc_hedr=SKN,
+           ncdc_hedrfuzzy=SKNa,
+           temp_hedr=SKN2,
+           temp_hedrfuzzy=SKN2a,
+           jon_hedr=SKN3,
+           jon_hedrfuzzy=SKN3a,
+           rf_hedr=SKN4,
+           rf_hedrfuzzy=SKN4a,
+           rf_2015=SKN5,
+           rf_2015fuzzy=SKN5a,
+           rfun_2015=SKN6,
+           rfun_2015fuzzy=SKN6a)
+
+## Compare ghcnd (688) locations to matched SKN #s
+merged2<-cbind(dat, out)
+write.csv(merged2, "../NCDCStationKeys/SKNcomparisonMay2018_298.csv", row.names=F)
+
+#####################################################################################
+## IN EXCEL: For each station, manually match the SKN that converges across multiple 
+##   files for a given name.  Where no SKN pops up, manually compare with the two  
+##   RF atlas files for matching dates/similar location and elevation.
+## ADD in HaleNet station data/SKN values here!! (could have added them prior)
+## Save file as "MatchedSKN_ghcnd.csv"-- includes overall station stats
+#####################################################################################
+
+##############################################################
+##  Extract locations from the Rainfall Atlas site listing  ##
+##  Built off: ExtractLocations3.R                          ##
+##############################################################
+setwd("C:/Users/Aurora/OneDrive/Documents/Projects/TempMapping/NCDCStationKeys")
+
+#matchedSKN<-read.csv("MatchedSKN_ghcnd.csv", stringsAsFactors=F)
+#matchedSKN<-read.csv("MatchedSKN_ghcnd2.csv", stringsAsFactors=F)
+#matchedSKN<-read.csv("MatchedSKN_ghcnd3.csv", stringsAsFactors=F)
+matchedSKN<-read.csv("MatchedSKN_ghcnd4.csv", stringsAsFactors=F)
+
+dat<-matchedSKN[,-c(13:26)]     # remove extraneous columns
+dat$SKN<-as.character(dat$SKN)  # set SKN to string, not numeric
+
+loc_RFused<-read.csv("RFFinalStationData_Used_201504pr.csv", stringsAsFactors=F)[,c(1:4, 14:17, 96, 95)]
+loc_RFunused<-read.csv("RFFinalStations_NotUsed_201504pr.csv", stringsAsFactors=F)[,c(1:4, 14:19)]
+loc_RF<-rbind(loc_RFused, loc_RFunused)
+loc_RF$SKN<-as.character(loc_RF$SKN)  # set SKN to string
+
+ghcnDLoc<-merge(dat, loc_RF, by="SKN", all.x=T)  # Merge dataframes but keep all 299 stations
+
+#write.csv(ghcnDLoc, file="StationLocationsApr2016.csv", row.names=F)
+#write.csv(ghcnDLoc, file="StationLocationsApr20162.csv", row.names=F)
+#write.csv(ghcnDLoc, file="StationLocationsApr20163.csv", row.names=F)
+write.csv(ghcnDLoc, file="StationLocationsApr201805.csv", row.names=F)
+
+###############################################################################
+## In Excel, compare elevations from NCDC data and RF atlas data; compare dates of 
+##   observation and temp vs elevation for outliers, look for station mismatches
+## In ArcMap (see HITempStations.mxd), compare the ghcnd stations with 
+##   RF atlas stations over a DEM; note that ghcnd stations often wrong (in water)
+##
+##  ITERATE and rerun the above script until satisfied
+###############################################################################
+
+########################################################
+##  Generate an NCDC corrected location station key   ##
+########################################################
+##  Compare also to merged_mshr_NCDCstation299.csv;
+compr<-read.csv("../NCDC_DailyTemp/merged_mshr_NCDCstation298_rmwhtspc.csv")[,-c(2:12, 15, 17, 26)] # remove extraneous
+master<-merge(ghcnDLoc, compr, by="ghcnd_id", all.x=T)
+head(master)
+
+## Very little difference between ghcnd station records and mshr last records: GOOD
+diffLat<-master$lat-master$lat_dec; max(abs(diffLat), na.rm=T)
+diffLon<-master$long-master$lon_dec; max(abs(diffLon), na.rm=T)
+## Elev not examined since in feet
+## But how do these line up with DEMs??  Not always so good.
+
+##  Identify higher precision and better documented stations for GHCND (mshr)
+which(master$precis=="DDMMSS" & master$datumh=="NAD83")
+precision<-nchar(as.character(master$precis))>5 #DDMMSS, DDdddd, DDddddd
+datumNAD83<-master$datumh=="NAD83" 
+datumOldHi<-master$datumh=="OLD HAWAIIAN"
+hq<-precision & (datumNAD83|datumOldHi)
+
+##  Use ALL RF atlas values for Lat/Long for each station
+master$CorrElev_m<-master$ElevM
+master$CorrLat_DD<-master$Lat_DD
+master$CorrLong_DD<-master$Lon_DD
+
+##  Identify missing stations not documented by RF Atlas
+##  Use ghcnd locations for these
+miss<-is.na(master$Name)
+  notWGS<-master[miss,]
+  write.csv(notWGS, file="notWGS.csv", row.names=F)
+
+master$ghcn<-master$precis; master$ghcn[!miss]<-NA    
+master$CorrElev_m[miss]<-master$elev.m[miss]
+master$CorrLat_DD[miss]<-master$lat[miss]
+master$CorrLong_DD[miss]<-master$long[miss]
+		
+names(master)[9]<-"MoObs"
+names(master)[10:13]<-paste("ghcnd_", names(master)[10:13], sep="")
+names(master)[14:22]<-paste("rfa_", names(master)[14:22], sep="")
+#write.csv(master, "NCDCkey_Apr2016.csv", row.names=F)
+#write.csv(master, "NCDC_HNkey_Jun2017.csv", row.names=F)
+write.csv(master, "NCDC_HNkey_May2018.csv", row.names=F)
+
+#############################################################
+##  In Excel, add the NOAA-ESRL-GMD MLO station 
+##  For station metadata: http://www.esrl.noaa.gov/gmd/dv/site/site.php?code=MLO
+##   mlo<-data.frame(Station	   = "MLO", Elevation	 = 3397.00,
+##                Latitude	 = 19.5362, Longitude	 = -155.5763)
+##  Save file of all stations as ## StationKey_all.csv ##
+##  Next script:
+
+#############################################################
+##  In Excel, remove Midway and French Frigate Shoals (total stations 309)
+## NCDC: 297, HN: 11, MLO: 1
